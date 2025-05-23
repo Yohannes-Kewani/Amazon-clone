edef233dd8733d4331f5da5195715555914df728
Yohannes-Developer <yohanneskewani@mu.edu.et>
2025-05-23 12:50:42 +0300

    Carousel effect is added to images below the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Components/Header/Header.jsx";
 import "./App.css";
+import Carousel from "./Components/Carousel/CarouselEffect.jsx";
 
 function App() {
 
   return (
     <>
       <Header />
+      <Carousel />
     </>
   );
 }
