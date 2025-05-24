d751a19d816156299dcdfd1d33297284038bebf2
Yohannes-Developer <yohanneskewani@mu.edu.et>
2025-05-24 17:44:28 +0300

    category routing

--- src/Pages/Results/Results.jsx
+++ src/Pages/Results/Results.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import Layout from "../../Components/Layout/Layout";
+import { useParams } from "react-router-dom";
+import { ProductUrl } from "../../API/endpoints";
+import axios from "axios";
+import ProductCard from "../../Components/Product/ProductCard";
+import classes from "./Results.module.css";
 function Results() {
+  const [results, setResults] = useState([]);
+  const { categoryName } = useParams();
+  useEffect(() => {
+    axios
+      .get(`${ProductUrl}/products/category/${categoryName}`)
+      .then((res) => setResults(res.data))
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
-    <div>Results</div>
-  )
+    <Layout>
+      <section>
+        <h1 style={{ padding: "30px" }}>Results</h1>
+        <p style={{ padding: "30px" }}>Category/{categoryName}</p>
+        <hr />
+        <div className={classes.products_container}>
+          {results?.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </section>
+    </Layout>
+  );
 }
 
-export default Results
+export default Results;
