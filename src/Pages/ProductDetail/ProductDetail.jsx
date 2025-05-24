9a3a45fe83077a5a99f6a657aa21eaca3578499b
Yohannes-Developer <yohanneskewani@mu.edu.et>
2025-05-24 18:21:27 +0300

    Detail Product routing

--- src/Pages/ProductDetail/ProductDetail.jsx
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import Layout from '../../Components/Layout/Layout'
+import React, { useState, useEffect } from "react";
+import Layout from "../../Components/Layout/Layout";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { ProductUrl } from "../../API/endpoints";
+import ProductCard from "../../Components/Product/ProductCard";
 
 function ProductDetail() {
+  const { ProductId } = useParams();
+  const [product, setproduct] = useState({});
+  useEffect(() => {
+    axios
+      .get(`${ProductUrl}/products/${ProductId}`)
+      .then((res) => {
+        setproduct(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      }, []);
+  });
+
   return (
-    <Layout>ProductDetail</Layout>
-  )
+    <Layout>
+      <ProductCard key={product.id} product={product} />
+    </Layout>
+  );
 }
 
-export default ProductDetail
+export default ProductDetail;
--- src/Routing.jsx
+++ src/Routing.jsx
@@ -6,15 +6,17 @@ import Payment from "./Pages/Payment/Payment";
 import Orders from "./Pages/Orders/Orders";
 import Cart from "./Pages/Cart/Cart";
 import Results from "./Pages/Results/Results";
+import ProductDetail from "./Pages/ProductDetail/ProductDetail"
 function Routing() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Landing/>} />
+        <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<Signup />} />
         <Route path="/payments" element={<Payment />} />
         <Route path="/orders" element={<Orders />} />
-        <Route path="/category/:categoryName" element={<Results />}/>
+        <Route path="/category/:categoryName" element={<Results />} />
+        <Route path="/products/:ProductId" element={<ProductDetail />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
